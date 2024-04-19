39b55918373c4fd0a43d82d9c6a09aa517ee9b0e
a23kevre <a23kevre@student.his.se>
2024-04-19 10:49:40 +0200

    lagt till mountain som en arraylista i mainactivity.

--- app/src/main/java/com/example/networking/RecyclerViewItem.java
+++ app/src/main/java/com/example/networking/RecyclerViewItem.java
@@ -1,8 +1,5 @@
 package com.example.networking;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 @SuppressWarnings("WeakerAccess")
 public class RecyclerViewItem {
 
@@ -15,9 +12,4 @@ public class RecyclerViewItem {
     public String getTitle() {
         return title;
     }
-    ArrayList<RecyclerViewItem> items = new ArrayList<>(Arrays.asList(
-            new RecyclerViewItem("Matterhorn"),
-            new RecyclerViewItem("Mont Blanc"),
-            new RecyclerViewItem("Denali")
-    ));
 }
