f754f40dc2593d504b2b5319f672c4f7defa5505
a23kevre <a23kevre@student.his.se>
2024-04-22 16:26:28 +0200

    lagt till internet och visat namnen på bergen från url i recyclerview.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -8,39 +8,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private Gson gson;
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
     ArrayList<Mountain> items = new ArrayList<>();
     ArrayList<RecyclerViewItem> recyclerItems = new ArrayList<>();
-
     RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        RecyclerView view = findViewById(R.id.recycler_view);
-        view.setLayoutManager(new LinearLayoutManager(this));
-        view.setAdapter(adapter);
-
-        new JsonFile(this, this).execute(JSON_FILE);
+        gson = new Gson();
 
         items.add(new Mountain("Matterhorn"));
         items.add(new Mountain("Mont Blanc"));
         items.add(new Mountain("Denali"));
 
-        /*for(RecyclerViewItem mountain: items){
-            Log.d("Unique ID", mountain.toString()+" hej");
-        }
-        */
         for(int i=0; i<items.size(); i++){
             Log.d("Unique ID", items.get(i).toString()+" hej");
+            recyclerItems.add(new RecyclerViewItem(items.get(i).toString()));
         }
         adapter = new RecyclerViewAdapter(this, recyclerItems, new RecyclerViewAdapter.OnClickListener() {
             @Override
@@ -48,11 +45,27 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
                 Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
+
+        //new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
+        Log.d("===>", json);
+        Type type = new TypeToken<List<Mountain>>() {}.getType();
+        List<Mountain> listOfMountains = gson.fromJson(json, type);
+        recyclerItems.clear();
+        items.clear();
+        items.addAll(listOfMountains);
+        for(int i=0; i<items.size(); i++){
+            Log.d("===>1111", items.get(i).toString()+" hej");
+            recyclerItems.add(new RecyclerViewItem(items.get(i).toString()));
+        }
+        adapter.notifyDataSetChanged();
     }
-
 }
